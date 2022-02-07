04db752beaae4af973c0df7e27fe4258adf1b62e
Alex Harris <a.harris31@outlook.com>
2022-02-07 17:10:53 +0000

    Fix room tests to throw correct exception

--- Application.Tests/RoomTests.cs
+++ Application.Tests/RoomTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Application.Exceptions;
+using System;
 using Xunit;
 
 namespace Application.Tests
@@ -26,7 +27,7 @@ namespace Application.Tests
         {
             Action action = () => new Room(length, width, height);
 
-            Assert.Throws<ArgumentOutOfRangeException>(action);
+            Assert.Throws<DimensionOutOfRangeException>(action);
         }
     }
 }
