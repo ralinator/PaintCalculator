3d02c3cd864cb2d535bd83eb6fe495a73c901a18
Alex Harris <a.harris31@outlook.com>
2022-02-07 17:56:45 +0000

    Add room wall area calculator

---
+++ Application.Tests/RoomWallAreaCalculatorTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace Application.Tests
+{
+    public class RoomWallAreaCalculatorTests
+    {
+        [Fact]
+        public void Calculate_ValidRoom_ReturnsTotalWallArea()
+        {
+            const decimal length = 2;
+            const decimal width = 3;
+            const decimal height = 4;
+
+            decimal singleLengthWallExpectedArea = length * height;
+            decimal bothLengthWallsExpectedArea = singleLengthWallExpectedArea * 2;
+
+            decimal singleWidthWallExpectedArea = width * height;
+            decimal bothWidthWallsExpectedArea = singleWidthWallExpectedArea * 2;
+            decimal expectedTotalArea = bothLengthWallsExpectedArea + bothWidthWallsExpectedArea;
+
+            var room = new Room(length, width, height);
+            var sut = new RoomWallAreaCalculator();
+
+            var actual = sut.Calculate(room);
+
+            Assert.Equal(expectedTotalArea, actual);
+        }
+    }
+}
---
+++ Application/RoomWallAreaCalculator.cs
@@ -0,0 +1,21 @@
+﻿namespace Application
+{
+    public class RoomWallAreaCalculator
+    {
+        public decimal Calculate(Room room)
+        {
+            var lengthWallsArea = CalculateWallsArea(room.Length, room.Height);
+            var widthWallsArea = CalculateWallsArea(room.Width, room.Height);
+            var totalArea = lengthWallsArea + widthWallsArea;
+            return totalArea;
+        }
+
+        private decimal CalculateWallsArea(decimal groundDimension, decimal heightOfRoom)
+        {
+            var areaCalculator = new AreaCalculator();
+            var singleWallArea = areaCalculator.Calculate(groundDimension, heightOfRoom);
+            var bothWallsArea = singleWallArea * 2;
+            return bothWallsArea;
+        }
+    }
+}
