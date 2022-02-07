9fa19f38fe55313bb140c4bfdb82461434e57df5
Alex Harris <a.harris31@outlook.com>
2022-02-07 14:55:41 +0000

    Add area calculator and basic test

---
+++ Application.Tests/AreaCalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace Application.Tests
+{
+    public class AreaCalculatorTests
+    {
+        private readonly AreaCalculator _sut;
+        public AreaCalculatorTests()
+        {
+            _sut = new AreaCalculator();
+        }
+        [Fact]
+        public void Calculate_PositiveInputs_ReturnsArea()
+        {
+            var actual = _sut.Calculate(2, 3);
+
+            Assert.Equal(6, actual);
+        }
+    }
+}
---
+++ Application/AreaCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Application
+{
+    public class AreaCalculator
+    {
+        public decimal Calculate(decimal length, decimal width)
+        {
+
+        }
+    }
+}
