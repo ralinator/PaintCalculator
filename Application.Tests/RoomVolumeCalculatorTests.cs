a1b5a2f303b1e6b02f99b199f08dfd3db39f1f2f
Alex Harris <a.harris31@outlook.com>
2022-02-07 19:25:53 +0000

    Add room volume calculator and tests

---
+++ Application.Tests/RoomVolumeCalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace Application.Tests
+{
+    public class RoomVolumeCalculatorTests
+    {
+        [Fact]
+        public void Calculate_ValidRoom_ReturnsVolume()
+        {
+            decimal length = 2;
+            decimal width = 3;
+            decimal height = 4;
+            var room = new Room(length, width, height);
+            var sut = new RoomVolumeCalculator();
+
+            var actual = sut.Calculate(room);
+
+            Assert.Equal(24, actual);
+        }
+    }
+}
---
+++ Application/RoomVolumeCalculator.cs
@@ -0,0 +1,12 @@
+﻿namespace Application
+{
+    public class RoomVolumeCalculator
+    {
+        public decimal Calculate(Room room)
+        {
+            var volumeCalculator = new VolumeCalculator();
+            var volume = volumeCalculator.Calculate(room.Length, room.Width, room.Height);
+            return volume;
+        }
+    }
+}
