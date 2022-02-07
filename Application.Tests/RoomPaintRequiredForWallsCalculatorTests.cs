154482298e832efaac3ce6335ded24f80ee2bbc9
Alex Harris <a.harris31@outlook.com>
2022-02-07 20:12:39 +0000

    Add room paint required for walls calculator and tests

---
+++ Application.Tests/RoomPaintRequiredForWallsCalculatorTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace Application.Tests
+{
+    public class RoomPaintRequiredForWallsCalculatorTests
+    {
+        [Fact]
+        public void Calculate_ValidRoom_ReturnsTotalWallArea()
+        {
+            const decimal length = 2;
+            const decimal width = 3;
+            const decimal height = 4;
+
+            var room = new Room(length, width, height);
+            var wallAreaCalculator = new RoomWallAreaCalculator();
+            var wallArea = wallAreaCalculator.Calculate(room);
+            const decimal wallSurfaceAreaToVolumeOfPaintRatio = 10;
+            var expected = wallArea / wallSurfaceAreaToVolumeOfPaintRatio;
+            var sut = new RoomPaintRequiredForWallsCalculator(wallSurfaceAreaToVolumeOfPaintRatio);
+
+            var actual = sut.Calculate(room);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Application/RoomPaintRequiredForWallsCalculator.cs
@@ -0,0 +1,20 @@
+﻿namespace Application
+{
+    public class RoomPaintRequiredForWallsCalculator
+    {
+        private readonly decimal _wallSurfaceAreaToVolumeOfPaintRatio;
+
+        public RoomPaintRequiredForWallsCalculator(decimal wallSurfaceAreaToVolumeOfPaintRatio = 10)
+        {
+            _wallSurfaceAreaToVolumeOfPaintRatio = wallSurfaceAreaToVolumeOfPaintRatio;
+        }
+
+        public decimal Calculate(Room room)
+        {
+            var roomWallAreaCalculator = new RoomWallAreaCalculator();
+            var wallArea = roomWallAreaCalculator.Calculate(room);
+            var paintVolume = wallArea / _wallSurfaceAreaToVolumeOfPaintRatio;
+            return paintVolume;
+        }
+    }
+}
