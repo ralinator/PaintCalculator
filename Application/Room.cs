3cd9d14d3e168c4c0ae3836650c52ba7fca4a109
Alex Harris <a.harris31@outlook.com>
2022-02-07 15:24:37 +0000

    Add room class

---
+++ Application/Room.cs
@@ -0,0 +1,16 @@
+﻿namespace Application
+{
+    public class Room
+    {
+        public Room(decimal length, decimal width, decimal height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public decimal Length { get; set; }
+        public decimal Width { get; set; }
+        public decimal Height { get; set; }
+    }
+}
